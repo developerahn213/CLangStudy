f7063bd30d2b830acbb5620bc605f6c0b252761c
developerahn213 <ahnvictoire213@gmail.com>
2024-11-06 00:44:15 +0900

    bitwise operation

--- BasicPractice/Practice.c
+++ BasicPractice/Practice.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello World");
+    printf("Hello World\n");
+    int a = 21; // 0001 0101(binary)
+    a = a | (1<<3);
+    int b = (1<<2); // print 4(decimal), 100(binary)
+    int c = b<<1; // It means multiple 2
+    c <<= 1;
+    printf("The value of a is %d\n", a);
+    printf("The value of b is %d\n", b);
+    printf("The value of c is %d\n", c);
 
     return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-#About
-The purpose of this repository is to make the log of study.
+##About
+The purpose of this repository is to remain the study log.
